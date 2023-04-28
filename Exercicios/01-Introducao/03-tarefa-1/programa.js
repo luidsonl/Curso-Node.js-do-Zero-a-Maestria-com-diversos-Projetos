f89313f419ead084b7557fad861352806fdb996f
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-04-28 19:28:41 -0300

    update

---
+++ Exercicios/01-Introducao/03-tarefa-1/programa.js
@@ -0,0 +1,12 @@
+/**
+ * 1-Crie um novo projeto de Node.js
+ * 2-Crie um arquivo para a aplicação com o nome programa
+ * 3-no arquivo crie duas variáveis e imprima a soma delas
+ * 4-Execute o arquivo e verifique a resposta no terminal
+ */
+
+let a = 10;
+let b = 20;
+
+
+console.log(a+b);
