37f164083ec9d7d673d8f5a8db7986f5c15e2bea
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-05-02 22:24:26 -0300

    update-seção3

---
+++ Exercicios/03-core-modules/02-retornando-html/index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const port = 3000
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200
+    res.setHeader('Contenty-type', 'text/html')
+    res.end('<h1>Olá, HTML</h1>')
+})
+
+server.listen(port,()=>{
+    console.log('Servidor rodando na porta: %s', port)
+})
