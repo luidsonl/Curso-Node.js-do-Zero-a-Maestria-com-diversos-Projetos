c1a8b22901b53de797aaa8906b2a0f2cc6b81b6d
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-05-14 17:36:02 -0300

    update seção 7

---
+++ Exercicios/07-template-engine/05-else/index.mjs
@@ -0,0 +1,32 @@
+import express from 'express'
+import exphbs from 'express-handlebars'
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res)=>{
+    res.render('dashboard')
+})
+
+app.get('/', (req, res)=>{
+    
+    const user = {
+        name: 'Luidson',
+        surname: 'Lima',
+        age: 23
+    }
+    const texto = 'Luidson está aprendendo Node.js e é mais fácil que o phpta'
+
+    const auth = true
+
+    const approved = true
+    
+    //user : user *apelido o parâmetro
+    res.render('home', {user : user, texto, auth, approved})
+})
+
+app.listen(3000,()=>{
+    console.log('App rodando')
+})
