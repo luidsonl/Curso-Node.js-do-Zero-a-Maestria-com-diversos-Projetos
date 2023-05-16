dade27c3e591c5769bb8134d35a3cd24b29f24c7
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-05-16 12:21:01 -0300

    update - seção 8

---
+++ Exercicios/08-node-mysql/01-instalacao-mysql/index.mjs
@@ -0,0 +1,29 @@
+import express from 'express'
+import exphbs from 'express-handlebars'
+import mysql from 'mysql'
+import path from 'path'
+import connection from './connection.mjs'
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+const conn = mysql.createConnection(connection)
+
+conn.connect((err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('Conexão feita com sucesso')
+    }
+    app.listen(3000)
+})
