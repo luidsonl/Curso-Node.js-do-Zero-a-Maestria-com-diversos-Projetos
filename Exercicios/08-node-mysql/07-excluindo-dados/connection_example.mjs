973a166739d38c0680451f8f59e60148a0125b1f
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-05-20 00:02:50 -0300

    update - seção 8

---
+++ Exercicios/08-node-mysql/07-excluindo-dados/connection_example.mjs
@@ -0,0 +1,10 @@
+const connection = {
+    host:'localhost',
+    user: 'admin',
+    password: '123456',
+    database: 'nodemysql'
+}
+
+export default connection
+
+//Configurar e renomear arquivo para conection.mjs
