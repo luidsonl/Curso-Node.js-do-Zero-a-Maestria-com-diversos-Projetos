6fafc53a618206230c0e6c49d12f64fb2ea53dc9
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-05-26 17:06:43 -0300

    update seção 9

---
+++ Exercicios/09-sequelize/10-add-relacionado/models/Adress.mjs
@@ -0,0 +1,34 @@
+import { DataTypes } from "sequelize";
+import db from '../db/conn.mjs'
+import User from "./User.mjs";
+
+const Address = db.define('address', {
+    street:{
+        type: DataTypes.STRING,
+        required: true,
+    },
+    number:{
+        type: DataTypes.STRING,
+        required: true,
+    },
+    neighborhood:{
+        type: DataTypes.STRING,
+        required: true,
+    },
+    city:{
+        type: DataTypes.STRING,
+        required: true,
+    },
+    state:{
+        type: DataTypes.STRING,
+        required: true,
+    },
+    country:{
+        type: DataTypes.STRING,
+        required: true,
+    },   
+})
+
+Address.belongsTo(User)
+
+export default Address
