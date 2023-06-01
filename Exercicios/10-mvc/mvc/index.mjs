1acd278bbffebad7887d7bb0132bb3610d3ab8e7
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-05-31 22:52:45 -0300

    update - seção 10

---
+++ Exercicios/10-mvc/mvc/index.mjs
@@ -0,0 +1,19 @@
+import express from "express";
+import exphbs from 'express-handlebars'
+import conn from './db/conn.mjs'
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.listen(3000)
