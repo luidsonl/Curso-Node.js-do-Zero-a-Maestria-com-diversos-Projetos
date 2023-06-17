449d4158e9a8d79d1a5cc742b20e245db5f4ecdb
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-06-17 15:21:38 -0300

    update - seção 11

--- Projetos/11-thoughts/index.mjs
+++ Projetos/11-thoughts/index.mjs
@@ -5,12 +5,48 @@ import FileStoreModule from "session-file-store"
 import flash from "express-flash"
 import conn from "./db/conn.mjs"
 
-const FileStore = FileStoreModule(session);
+import sessionMiddleware from "./middlewares/sessionConfig.mjs"
+
+
 
 const app = express()
 
+//Template engine
+app.engine('handlebars' , exphbs.engine())
+app.set('view engine', 'handlebars')
+
+//Respostas do body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+// Middlewares
+app.use(
+    sessionMiddleware
+)
+
+// Public path
+app.use(express.static('public'))
+
+// Flash messages
+app.use(flash())
+
+// configurar sessão para resposta
+app.use((req, res, next)=>{
+    if(req.session.userId){
+        res.locals.session = req.session
+    }
+    next()
+})
+
+
 conn.sync().then(()=>{
     app.listen(3000)
+
 }).catch((err)=>{
     console.log(err)
 })
---
+++ Projetos/11-thoughts/middlewares/sessionConfig.mjs
@@ -0,0 +1,32 @@
+import session from 'express-session';
+import FileStoreModule from "session-file-store"
+import path from 'path';
+import os from 'os';
+
+const FileStore = FileStoreModule(session);
+
+const sessionMiddleware = session({
+    name: 'session',
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+        logFn: function () {},
+        path: path.join(os.tmpdir(), 'sessions') // Caminho para salvar os arquivos de sessão
+    }),
+
+    cookie:{
+        secure:false,
+        maxAge:360000,
+        expires: new Date(Date.now() + 360000),
+        httpOnly:true
+    }
+});
+
+
+/*
+const test= path.join(os.tmpdir(), 'sessions')
+console.log(test)
+*/
+
+export default sessionMiddleware;
