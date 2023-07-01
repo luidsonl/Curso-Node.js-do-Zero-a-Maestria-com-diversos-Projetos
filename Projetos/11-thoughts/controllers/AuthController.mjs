706440af90250ab06ee9a0e08981b059058b3fab
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-07-01 00:23:39 -0300

    update seção 11

--- Projetos/11-thoughts/controllers/AuthController.mjs
+++ Projetos/11-thoughts/controllers/AuthController.mjs
@@ -1,3 +1,4 @@
+import { where } from "sequelize"
 import User from "../models/User.mjs"
 import bcrypt from 'bcryptjs'
 
@@ -12,17 +13,53 @@ class AuthController{
     static async registerPost(req, res){
         const {name, email, password, passwordConfirm} = req.body
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        let error = false
 
         if(passwordConfirm != password){
             req.flash('message', 'A confirmação de senha não bate')
+            error = true
         }
 
         if(!emailRegex.test(email)){
             req.flash('message', 'Formato inválido de e-mail')
+            error = true
         }
 
-        res.render('auth/register', req.body)
+        if(await User.findOne({where:{email: email}})){
+            req.flash('message', 'Email já está em uso')
+            error = true
+        }
+
+        if(error){
+            res.render('auth/register', req.body)
         return
+        }
+        
+        //Criação de senha
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        try{
+            const createdUser =  await User.create(user)
+
+            //Inicializando session
+            req.session.userId = createdUser.id
+
+            req.flash('message', 'Cadastro realizado com sucesso')
+
+            req.session.save(()=>{
+                res.redirect('/')
+            })
+
+        }catch(err){
+            console.log(err)
+        }
     }
 }
 
