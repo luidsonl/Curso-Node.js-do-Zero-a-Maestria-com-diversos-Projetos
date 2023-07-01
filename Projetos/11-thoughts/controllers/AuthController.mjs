02a3d5343c70402438a2bd053b33de90879de1e9
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-06-30 23:19:35 -0300

    update - seção 11

--- Projetos/11-thoughts/controllers/AuthController.mjs
+++ Projetos/11-thoughts/controllers/AuthController.mjs
@@ -1,5 +1,5 @@
 import User from "../models/User.mjs"
-
+import bcrypt from 'bcryptjs'
 
 class AuthController{
     static login(req, res){
@@ -7,6 +7,22 @@ class AuthController{
     }
     static register(req, res){
         res.render('auth/register')
+    }
+
+    static async registerPost(req, res){
+        const {name, email, password, passwordConfirm} = req.body
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(passwordConfirm != password){
+            req.flash('message', 'A confirmação de senha não bate')
+        }
+
+        if(!emailRegex.test(email)){
+            req.flash('message', 'Formato inválido de e-mail')
+        }
+
+        res.render('auth/register', req.body)
+        return
     }
 }
 
