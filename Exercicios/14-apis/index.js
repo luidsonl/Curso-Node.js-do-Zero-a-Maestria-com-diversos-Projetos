453892030845eec3b1e38eaee9e47a22da69db49
Luidson Lima Santos <luidsonlsti@gmail.com>
2024-10-02 23:10:19 -0300

    update

--- Exercicios/14-apis/index.js
+++ Exercicios/14-apis/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 // endpoints
 app.get('/', (req, res)=>{
-    res.json({
+    res.status(200).json({
         message: 'olá mundo'
     })
 })
@@ -21,7 +21,13 @@ app.post('/product', (req, res)=>{
     const price = req.body.price
     const name = req.body.name
 
-    res.json({
+    if(!name){
+        res.status(422).json({
+            message: "o campo 'nome' é obrigatório"
+        })
+    }
+
+    res.status(201).json({
         message: `produto ${name} que custa ${price} foi cadastrado com sucesso`
     })
 })
