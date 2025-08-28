6e99de08e9811532462ec914bd73e7d7f74b4367
Luidson Lima Santos <luidsonlsti@gmail.com>
2025-08-28 20:17:03 -0300

    update

--- Projetos/15-get-a-card/frontend/src/pages/CardPage/index.jsx
+++ Projetos/15-get-a-card/frontend/src/pages/CardPage/index.jsx
@@ -1,9 +1,37 @@
-function CardPage(){
-    return(
-        <section>
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import CardService from "../../services/CardService";
+import MediaService from "../../services/MediaService";
 
-        </section>
-    )
+function CardPage() {
+  const { id } = useParams();
+  const [card, setCard] = useState(null);
+
+  useEffect(() => {
+    if (!id) return;
+
+    async function getCard() {
+      try {
+        const cardData = await CardService.getOne(id);
+        setCard(cardData);
+        console.log(cardData);
+      } catch (err) {
+        console.error('Erro ao buscar card:', err);
+      }
+    }
+
+    getCard();
+  }, [id]);
+
+  if (!card) return <p>Carregando...</p>;
+
+  return (
+    <section>
+        <h1>{card.name}</h1>
+        <img src={MediaService.getUrl(card.featuredImage.filePath)} alt="" />
+        <p>{card.description}</p>
+    </section>
+  );
 }
 
 export default CardPage;
